2e1c6932c543dd38e0c1d292c957c0e08d9047ac
LHTrungSkySP <lehoangtrung038582@gmail.com>
2024-12-28 16:58:34 +0700

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,25 +5,7 @@ import { AppComponent } from './app.component';
 const routes: Routes = [
   {
     path: '',
-    // component: AppComponent,
-    children: [
-      {
-        path: 'home',
-        loadChildren: () => import('./components/layout-components/home/home.module').then(m => m.HomeModule)
-      },
-      {
-        path: '',
-        loadChildren: () => import('./components/layout-components/home/home.module').then(m => m.HomeModule)
-      },
-      {
-        path: ':category',
-        loadChildren: () => import('./components/layout-components/product-home/product-home.module').then(m => m.ProductHomeModule)
-      },
-      {
-        path: ':category/:product-name',
-        loadChildren: () => import('./components/layout-components/product-detail/product-detail.module').then(m => m.ProductDetailModule)
-      }
-    ]
+    component: AppComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderModule } from "./components/layout-components/header/header.module";
-import { SupportCenterModule } from './components/layout-components/support-center/support-center.module';
 
 @NgModule({
   declarations: [
@@ -15,8 +13,6 @@ import { SupportCenterModule } from './components/layout-components/support-cent
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HeaderModule,
-    SupportCenterModule
 ],
   providers: [],
   bootstrap: [AppComponent]
